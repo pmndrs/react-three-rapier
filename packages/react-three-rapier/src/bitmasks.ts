2353b888baea00c3149422f5bc9651b57dbdb9e8
Hendrik Mans <hendrik@mans.de>
2022-09-10 16:39:51 +0200

    Docs

--- packages/react-three-rapier/src/bitmasks.ts
+++ packages/react-three-rapier/src/bitmasks.ts
@@ -4,31 +4,31 @@ import { InteractionGroups } from "@dimforge/rapier3d-compat";
  * Start creating a collision bitmask, starting with the groups
  * the object is in.
  *
- * @param group One or more numerical layer identifiers.
+ * @param groupLayers One or more numerical layer identifiers.
  * @returns A builder object with additional methods for configuring collisions.
  */
-export const collide = (...group: number[]) => ({
+export const collide = (...groupLayers: number[]) => ({
   /**
    * Collide the object with other groups.
    *
-   * @param mask One or more numerical layer identifiers.
+   * @param maskLayers One or more numerical layer identifiers.
    * @returns A bitmask representing the collision configuration.
    */
-  with: (...mask: number[]) => (bitmask(...group) << 16) + bitmask(...mask),
+  with: (...maskLayers: number[]) => group(...groupLayers) + mask(...maskLayers),
 
   /**
    * Collide the object with all other groups.
    *
    * @returns A bitmask representing the collision configuration.
    */
-  withEverything: () => (bitmask(...group) << 16) + 0b11111111,
+  withEverything: () => group(...groupLayers) + 0b11111111,
 
   /**
    * Collide the object with nothing.
    *
    * @returns A bitmask representing the collision configuration.
    */
-  withNothing: () => (bitmask(...group) << 16)
+  withNothing: () => group(...groupLayers)
 })
 
 /**
@@ -40,5 +40,18 @@ export const collide = (...group: number[]) => ({
 export const bitmask = (...layers: number[]): InteractionGroups =>
   layers.reduce((acc, layer) => acc | (1 << layer), 0);
 
+/**
+ * Convert the specified numerical layers into a bitmask representing the
+ * group association part in Rapier's collision and solver groups properties.
+ *
+ * @param layers One or more numerical layer identifiers.
+ */
 export const group = (...layers: number[]): InteractionGroups => bitmask(...layers) << 16;
+
+/**
+ * Convert the specified numerical layers into a bitmask representing the
+ * mask part in Rapier's collision and solver groups properties.
+ *
+ * @param layers One or more numerical layer identifiers.
+ */
 export const mask = (...layers: number[]): InteractionGroups => bitmask(...layers);
