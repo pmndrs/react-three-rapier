6e832442307254ad893ed9710ec9478e7a9bef5b
Hendrik Mans <hendrik@mans.de>
2022-09-16 16:54:07 +0200

    fix: interactionGroups will handle groups from 0 to 15

--- packages/react-three-rapier/test/interactionGroups.test.ts
+++ packages/react-three-rapier/test/interactionGroups.test.ts
@@ -2,20 +2,20 @@ import { interactionGroups } from "../src/bitmasks"
 
 describe("interactionGroups", () => {
   it("returns the correct bitmask for the given memberships and filters", () => {
-    expect(interactionGroups([1], [1]))
+    expect(interactionGroups([0], [0]))
       .toBe(0b0000_0000_0000_0001_0000_0000_0000_0001)
 
-    expect(interactionGroups([1, 2], [1, 2, 3, 4]))
+    expect(interactionGroups([0, 1], [0, 1, 2, 3]))
       .toBe(0b0000_0000_0000_0011_0000_0000_0000_1111)
   })
 
   it("defaults the second argument to interact with all groups", () => {
-    expect(interactionGroups([1, 2]))
+    expect(interactionGroups([0, 1]))
       .toBe(0b0000_0000_0000_0011_1111_1111_1111_1111)
   })
 
   it("accepts scalar numerical values if only one group is referenced", () => {
-    expect(interactionGroups(2, [1, 2]))
+    expect(interactionGroups(1, [0, 1]))
       .toBe(0b0000_0000_0000_0010_0000_0000_0000_0011)
   })
 })
