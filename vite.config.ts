e955e2a9a23d8e5b0ffeb82d4d56701d1cee4117
Hugo Wiledal <hugo.wiledal@gmail.com>
2024-01-30 08:23:18 +0100

    fix: back to happy-dom

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import * as path from "path";
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "jsdom",
+    environment: "happy-dom",
     setupFiles: path.resolve(
       process.cwd(),
       "./packages/react-three-rapier/tests/setup.ts"
