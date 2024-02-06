92c4a23f8d4b4ef1a061eecfd1f23550f49902e8
0xtito <welovethepenguins@gmail.com>
2024-02-05 21:14:04 -0500

    feat(examples): added springs and rope examples

--- demo/src/examples/rope-joint/RopeJointExample.tsx
+++ demo/src/examples/rope-joint/RopeJointExample.tsx
@@ -1,4 +1,4 @@
-import { Sphere } from "@react-three/drei";
+import { Sphere, Box } from "@react-three/drei";
 import {
   BallCollider,
   RapierRigidBody,
@@ -8,25 +8,98 @@ import {
 } from "@react-three/rapier";
 import { useRef } from "react";
 import { Demo } from "../../App";
+import { Vector3 } from "@react-three/fiber";
 
-// todo: flesh out this demo
+const WALL_COLORS = ["#50514F", "#CBD4C2", "#FFFCFF", "#247BA0", "#C3B299"];
 
-const RopeJoint = (props: RigidBodyOptions) => {
+interface BoxRigidBodyProps extends RigidBodyOptions {
+  color: string;
+}
+
+interface BoxWallProps extends RigidBodyOptions {
+  height: number;
+  width: number;
+}
+
+interface RopeJointProps {
+  anchorPosition: Vector3;
+  ballPosition: Vector3;
+  ropeLength: number;
+}
+
+const Floor = (props: RigidBodyOptions) => {
+  return (
+    <RigidBody type="fixed" colliders="cuboid" position={[0, -1, 0]} {...props}>
+      <Box args={[20, 1, 20]}>
+        <meshStandardMaterial color="white" />
+      </Box>
+    </RigidBody>
+  );
+};
+
+const BoxRigidBody = (props: BoxRigidBodyProps) => {
+  return (
+    <RigidBody {...props}>
+      <Box castShadow receiveShadow>
+        <meshStandardMaterial color={props.color} />
+      </Box>
+    </RigidBody>
+  );
+};
+
+const BoxWall = ({ height, width, ...props }: BoxWallProps) => {
+  const wall = [];
+
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      const position: [number, number, number] = [j, i, 0];
+      wall.push(
+        <BoxRigidBody
+          key={`${i}-${j}`}
+          {...props}
+          density={2}
+          color={WALL_COLORS[i % 5]}
+          position={position}
+        />
+      );
+    }
+  }
+
+  return (
+    <group name="wall" rotation-y={-0.7853982} position={[-1.8, 0, -1.8]}>
+      {wall.map((box, i) => box)}
+    </group>
+  );
+};
+
+const RopeJoint = ({
+  anchorPosition,
+  ballPosition,
+  ropeLength
+}: RopeJointProps) => {
   const anchor = useRef<RapierRigidBody>(null);
   const ball = useRef<RapierRigidBody>(null);
 
-  useRopeJoint(anchor, ball, [[0, 0, 0], [0, 0, 0], 1]);
+  useRopeJoint(anchor, ball, [[0, 0, 0], [0, 0, 0], ropeLength]);
 
   return (
     <group>
-      <RigidBody ref={anchor} {...props} />
+      {/* Anchor */}
+      <RigidBody ref={anchor} position={anchorPosition} />
 
-      <RigidBody ref={ball} {...props} colliders={false}>
-        <Sphere scale={0.2} receiveShadow castShadow>
+      {/* Wrecking Ball */}
+      <RigidBody
+        position={ballPosition}
+        ref={ball}
+        restitution={1.2}
+        density={30}
+        colliders={false}
+      >
+        <Sphere args={[2]} receiveShadow castShadow>
           <meshStandardMaterial metalness={1} roughness={0.3} />
         </Sphere>
 
-        <BallCollider args={[0.2]} restitution={1.2} />
+        <BallCollider args={[2]} />
       </RigidBody>
     </group>
   );
@@ -34,8 +107,14 @@ const RopeJoint = (props: RigidBodyOptions) => {
 
 export const RopeJointExample: Demo = () => {
   return (
-    <group scale={3}>
-      <RopeJoint position={[0, 0, 0]} />
+    <group position={[0, 0, 0]} scale={3}>
+      <Floor />
+      <BoxWall height={10} width={6} />
+      <RopeJoint
+        ropeLength={35}
+        anchorPosition={[0, 15, 0]}
+        ballPosition={[-8, 15, 8]}
+      />
     </group>
   );
 };
--- demo/src/examples/spring/SpringExample.tsx
+++ demo/src/examples/spring/SpringExample.tsx
@@ -1,4 +1,4 @@
-import { Sphere } from "@react-three/drei";
+import { Box, Sphere } from "@react-three/drei";
 import {
   BallCollider,
   RapierRigidBody,
@@ -6,34 +6,108 @@ import {
   RigidBodyOptions,
   useSpringJoint
 } from "@react-three/rapier";
-import { useRef } from "react";
+import { forwardRef, useMemo, useRef } from "react";
 import { Demo } from "../../App";
+import { useForwardedRef } from "@react-three/rapier/src/hooks/use-forwarded-ref";
+import { vectorArrayToVector3 } from "@react-three/rapier/src/utils/utils";
 
-const Spring = (props: RigidBodyOptions) => {
-  const anchor = useRef<RapierRigidBody>(null);
-  const ball = useRef<RapierRigidBody>(null);
+const COLORS_ARR = ["#335C67", "#FFF3B0", "#E09F3E", "#9E2A2B", "#540B0E"];
 
-  useSpringJoint(anchor, ball, [[0, 0, 0], [0, 0, 0], 2, 20, 2]);
+interface BallSpringProps extends RigidBodyOptions {
+  jointNum: number;
+  total: number;
+}
 
+interface BoxRigidBodyProps extends RigidBodyOptions {
+  color: string;
+}
+
+const BoxRigidBody = ({ color, ...props }: BoxRigidBodyProps) => {
   return (
-    <group>
-      <RigidBody ref={anchor} {...props} />
-
-      <RigidBody ref={ball} {...props} colliders={false}>
-        <Sphere scale={0.2} receiveShadow castShadow>
-          <meshStandardMaterial metalness={1} roughness={0.3} />
-        </Sphere>
-
-        <BallCollider args={[0.2]} restitution={1.2} />
-      </RigidBody>
-    </group>
+    <RigidBody {...props} ccd canSleep={false}>
+      <Box castShadow receiveShadow>
+        <meshStandardMaterial color={color} />
+      </Box>
+    </RigidBody>
   );
 };
 
+const BallSpring = forwardRef<RapierRigidBody, BallSpringProps>(
+  (props, floorRef) => {
+    const floor = useForwardedRef(floorRef);
+    const ball = useRef<RapierRigidBody>(null);
+
+    let stiffness = 1.0e3;
+    let criticalDamping = 2.0 * Math.sqrt(stiffness * (props.mass ?? 1));
+    let dampingRatio = props.jointNum / (props.total / 2);
+    let damping = dampingRatio * criticalDamping;
+
+    const ballPos = props.position as THREE.Vector3;
+
+    if (!ballPos) {
+      throw new Error("BallSpring requires a position prop");
+    }
+
+    useSpringJoint(ball, floor, [
+      [0, 0, 0],
+      [ballPos.x, ballPos.y - 3, ballPos.z],
+      0,
+      stiffness,
+      damping
+    ]);
+
+    return (
+      <RigidBody
+        key={`spring-${props.jointNum}`}
+        {...props}
+        ref={ball}
+        ccd
+        name={`spring-${props.jointNum}`}
+        position={ballPos}
+        colliders={false}
+        canSleep={false}
+      >
+        <Sphere args={[0.5]} castShadow receiveShadow>
+          <meshStandardMaterial color="#E09F3E" />
+        </Sphere>
+        <BallCollider args={[0.5]} />
+      </RigidBody>
+    );
+  }
+);
+
 export const SpringExample: Demo = () => {
+  const floor = useRef<RapierRigidBody>(null);
+
+  const vectorArr = useMemo(() => {
+    return Array.from({ length: 30 }).map((_, i) => {
+      return vectorArrayToVector3([-20 + 1.5 * (i + 1), 7.5, -30]);
+    });
+  }, []);
+
   return (
-    <group scale={3}>
-      <Spring position={[0, 0, 0]} />
-    </group>
+    <>
+      <RigidBody ref={floor} position={[0, 0, 0]} type="fixed" />
+
+      {vectorArr.map((_, i) => (
+        <group key={`box-ball-${i}`}>
+          <BallSpring
+            key={`ball-${i}`}
+            ref={floor}
+            position={vectorArr[i]}
+            mass={1}
+            jointNum={i}
+            total={30}
+          />
+          <BoxRigidBody
+            key={`box-${i}`}
+            color={COLORS_ARR[i % 5]}
+            position={[vectorArr[i].x, vectorArr[i].y + 3, vectorArr[i].z]}
+            colliders="cuboid"
+            density={100}
+          />
+        </group>
+      ))}
+    </>
   );
 };
